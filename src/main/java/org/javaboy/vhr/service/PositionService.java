2e78b44e89ea6d6dfa3445c7a7038e89220d9563
git <2415964836@qq.com>
2020-03-15 21:38:40 +0800

    20200315 职位管理 完 59

---
+++ src/main/java/org/javaboy/vhr/service/PositionService.java
@@ -0,0 +1,36 @@
+package org.javaboy.vhr.service;
+
+import org.apache.ibatis.annotations.Param;
+import org.javaboy.vhr.mapper.PositionMapper;
+import org.javaboy.vhr.model.Position;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class PositionService {
+    @Autowired
+    PositionMapper positionMapper;
+    public List<Position> getAllPositions() {
+       return positionMapper.getAllPositions();
+    }
+    public Integer addPosition(Position position){
+        position.setEnabled(true);
+        position.setCreateDate(new Date());
+        return positionMapper.insertSelective(position);
+    }
+
+    public Integer updatePositions(Position position) {
+       return positionMapper.updateByPrimaryKeySelective(position);
+    }
+
+    public Integer deletePositionById(Integer id) {
+        return positionMapper.deleteByPrimaryKey(id);
+    }
+
+    public Integer deletePositionsByIds(@Param("ids")Integer[] ids) {
+        return  positionMapper.deletePositionsByIds( ids);
+    }
+}
