6df991ea83091cc2baae781fdca033940a3a3f01
git <2415964836@qq.com>
2020-03-16 23:06:14 +0800

    20200316 职称管理 后端接口写完 62

---
+++ src/main/java/org/javaboy/vhr/service/JobLevelService.java
@@ -0,0 +1,32 @@
+package org.javaboy.vhr.service;
+
+import org.javaboy.vhr.mapper.JobLevelMapper;
+import org.javaboy.vhr.model.JobLevel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+@Service
+public class JobLevelService {
+    @Autowired
+    JobLevelMapper joblevelMapper;
+    public List<JobLevel> getAllJobLevels() {
+        return  joblevelMapper.getAllJobLevels();
+    }
+
+    public Integer addJobLevel(JobLevel jobLevel) {
+        jobLevel.setCreateDate(new Date());
+        jobLevel.setEnabled(true);
+
+        return joblevelMapper.insertSelective(jobLevel);
+    }
+
+    public Integer updateJobLevelById(JobLevel jobLevel) {
+        return joblevelMapper.updateByPrimaryKeySelective(jobLevel);
+    }
+
+    public Integer deleteJobLevelById(Integer id) {
+        return  joblevelMapper.deleteByPrimaryKey(id);
+    }
+}
